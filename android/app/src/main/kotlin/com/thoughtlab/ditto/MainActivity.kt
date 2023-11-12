5f9beaa43e9730e0ac1aa3c4d91e043b257511b4
syaiful-aa <syaiful.salam@agriaku.com>
2023-11-12 12:13:00 +0700

    setup flavor

--- android/app/src/main/kotlin/com/thoughtlab/ditto/MainActivity.kt
+++ android/app/src/main/kotlin/com/thoughtlab/ditto/MainActivity.kt
@@ -1,6 +1,34 @@
 package com.thoughtlab.ditto
 
+import android.os.Build
+import android.os.Bundle
+import androidx.annotation.NonNull
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val configChannel = "config_channel"
+
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            configChannel
+        ).setMethodCallHandler { call, result ->
+            provideResult(call, result)
+        }
+    }
+
+    private fun provideResult(call: MethodCall, result: MethodChannel.Result) {
+        when (call.method) {
+            "currentFlavor" -> {
+                result.success(BuildConfig.FLAVOR)
+            }
+            else -> {
+                result.notImplemented()
+            }
+        }
+    }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  final MethodChannel platform = const MethodChannel('config_channel');
+
+  String _flavor = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _checkFlavor();
+  }
+
+  Future<void> _checkFlavor() async {
+    try {
+      final String result = await platform.invokeMethod('currentFlavor');
+      setState(() {
+        _flavor = result;
+      });
+    } catch (e) {
+      throw ('failed to invoke currentFlavor: $e');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(_flavor),
         ),
       ),
     );
